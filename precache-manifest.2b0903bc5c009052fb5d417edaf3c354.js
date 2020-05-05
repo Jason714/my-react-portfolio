4c7450fb5b2eb9570ce1ca4c560797f06c7e44a4
Jason Blake <jblakecodes@gmail.com>
2020-05-05 14:34:53 -0700

    Updates

---
+++ precache-manifest.2b0903bc5c009052fb5d417edaf3c354.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4dab135b7c75168172e56e8ac8a172",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "e8f83761c56c5d3cb445",
+    "url": "/my-react-portfolio/static/css/main.47c4ce1b.chunk.css"
+  },
+  {
+    "revision": "3af455e19eaec95674ce",
+    "url": "/my-react-portfolio/static/js/2.bf8e8d0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-portfolio/static/js/2.bf8e8d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f83761c56c5d3cb445",
+    "url": "/my-react-portfolio/static/js/main.07adfbc5.chunk.js"
+  },
+  {
+    "revision": "35f9e58eb32da499e28b",
+    "url": "/my-react-portfolio/static/js/runtime-main.880269f4.js"
+  },
+  {
+    "revision": "646da057d113dbfb9774641732a6d3d7",
+    "url": "/my-react-portfolio/static/media/Resume.646da057.pdf"
+  },
+  {
+    "revision": "46a473352d32675a02d14eddb5c35467",
+    "url": "/my-react-portfolio/static/media/book-search.46a47335.png"
+  },
+  {
+    "revision": "0691cb63609bc3ac033d2486c469a089",
+    "url": "/my-react-portfolio/static/media/claudio-schwarz.0691cb63.jpg"
+  },
+  {
+    "revision": "d3868f4068acc3e8bb8962e5067e05d3",
+    "url": "/my-react-portfolio/static/media/laptop.d3868f40.jpg"
+  },
+  {
+    "revision": "deb63d61ca0a86aac254427c7ce9621d",
+    "url": "/my-react-portfolio/static/media/note-taker.deb63d61.png"
+  },
+  {
+    "revision": "ac8b897c6807d1ed17057493946ff1d0",
+    "url": "/my-react-portfolio/static/media/profile-pic.ac8b897c.jpg"
+  },
+  {
+    "revision": "74cf9f7d56ccf85ffd3398d38dcdb159",
+    "url": "/my-react-portfolio/static/media/quiz-app-screenshot.74cf9f7d.png"
+  },
+  {
+    "revision": "8ba02a851b952209ed7197ec96a07e56",
+    "url": "/my-react-portfolio/static/media/restaurant-finder-screenshot.8ba02a85.png"
+  },
+  {
+    "revision": "6e41572732aa6dfedfb6da41db3634ff",
+    "url": "/my-react-portfolio/static/media/robert-horvick.6e415727.jpg"
+  },
+  {
+    "revision": "05586c003e04e3da3aeca4bc02a18e30",
+    "url": "/my-react-portfolio/static/media/sunset.05586c00.jpg"
+  },
+  {
+    "revision": "917ba6b9c354e196cef8a4a586379053",
+    "url": "/my-react-portfolio/static/media/weather-dashboard-screenshot.917ba6b9.png"
+  },
+  {
+    "revision": "65e56be2e224dc822b9bc9ea53e3e15c",
+    "url": "/my-react-portfolio/static/media/workout-dashboard.65e56be2.png"
+  }
+]);
