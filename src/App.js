ec67cb16bc2de8a1840bfbf0a652d8073487f52d
Jason Blake <jblakecodes@gmail.com>
2020-05-05 15:52:32 -0700

    minor changes for deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Projects from "./pages/projects";
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { HashRouter as Router } from "react-router-dom";
-
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
