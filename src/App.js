1c917c9e55158783795ddcaafb117eb217984c0c
Jason Blake <jblakecodes@gmail.com>
2020-05-07 20:59:59 -0700

    attempting to deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
+    <Router>
+      <div>
         <Nav />
-        <Switch>
-          <Route path="/" component={Home} />
-          <Link to="/home" />
-          <Link to="/about" />
-          <Link to="/projects" />
-          <Link to="/contact" />
-        </Switch>
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
         <Footer />
       </div>
     </Router>
